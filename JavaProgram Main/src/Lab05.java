5181cb655d8def57853bfed5b6366b8a165d1d59
KrwJmg <flook_11323@hotmail.com>
2022-12-08 11:53:48 +0700

    Lab05

---
+++ JavaProgram Main/src/Lab05.java
@@ -0,0 +1,28 @@
+
+public class Lab05 {
+
+	public static void main(String[] args) {
+		inputStudent();
+
+	}
+ 
+	public static void inputStudent() {
+		;
+	}
+	
+	public static boolean isLength() {
+		return " ";
+	}
+	
+	public static boolean isITStudent() {
+		return " ";
+	}
+	
+	public static boolean isITSubject() {
+		return " ";
+	}
+	
+	public static void displayData() {
+		;
+	}
+}
